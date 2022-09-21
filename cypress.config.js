a3fb06ac0de6460528cabd29a47b096269ec86e6
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2022-09-21 17:57:21 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'ff4o8b',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000/',
+    "reporter": "mochawesome",
+    "reporterOptions": {
+      "reportDir": "mochawesome-report",
+      "overwrite": false,
+      "html": false,
+      "json": true
+    }
   },
 });
